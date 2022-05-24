3687febf6cc5e1321619c2855038f274353ad8bf
viethoang <viethoang212001@gmail.com>
2022-05-24 09:15:48 +0700

    done

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb+srv://Admin:Azz123443215@cluster0.ilvl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "jkfasdjkfnjkn3242332",
+        "REFRESH_TOKEN_SECRET": "nwejkrnwejknrjk4273342",
+        "PAYPAL_CLIENT_ID": "AWPR1Vtpz2pdgzPnVf-blkTNVxI1khzJQG9mzM-lwTBfTcfpy60UKc55TtWx9vGWxGToL1exG8VF5AvK",
+        "CLOUD_UPDATE_PRESET": "nextjs_store",
+        "CLOUD_NAME": "viethoang",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/viethoang/image/upload"
+    }
+}
